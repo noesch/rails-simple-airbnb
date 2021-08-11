3488bd1248bf50d87018f279ac8be0bad48f8715
Janus Vollmer <janus.vollmer@gmail.com>
2021-08-11 15:43:35 +0200

    Added the Flat model, the routes and the controller and added 4 flats to the database using the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+
+private_paradise = { name:'Private Paradise', address: 'Cloud № 9', description: 'Your own private paradise. Sleep on your own cloud aqnd enjoy our outstanding view and havenly silence', price_per_night: 999, number_of_guests: 2}
+cozy_castle = { name:'Cozy Castle', address: 'Sherwood Forest 1', description: 'This old castle has history written all over its ancient walls. Marvel at the dungeon or fall in love with the tower.', price_per_night: 500, number_of_guests: 200}
+luxury_loft = { name:'Luxury Loft', address: 'Wallstreet 21', description: 'The most decadent luxury arpatment money can rent. Sleep where the stock market never does.', price_per_night: 100000000000, number_of_guests: 0}
+humble_hut = { name:'Humble Hut', address: 'Black Hill Forest', description: "It's not much, but it's yours. Some say you can hear the whispers of the local witch at night", price_per_night: 5, number_of_guests: 8}
+
+[private_paradise, cozy_castle, luxury_loft, humble_hut].each do |attributes|
+  flat = Flat.create!(attributes)
+  puts "Created #{flat.name}"
+end
